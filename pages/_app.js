81738b8a8d661ccf6c6067b4efda75eb3ad1d7c1
“Kojo <kodeon6@gmail.com>
2021-02-22 18:31:11 +0000

    Add navbar and data

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
+import Link from 'next/link'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/'>
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/about'>
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/projects'>
+              <a>Projects</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  ) 
 }
 
 export default MyApp
---
+++ pages/projects.js
@@ -0,0 +1,16 @@
+import Head from "next/head"
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>Projects</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+        <h1>My projects</h1>
+      </main>
+    </div>
+  )
+}
